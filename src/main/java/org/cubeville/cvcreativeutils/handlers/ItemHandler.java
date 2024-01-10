ae6c434e9ce339e8e2aca75f3ea3c46edd0817af
ToeDev <73237279+ToeDev@users.noreply.github.com>
2024-01-10 17:43:06 -0500

    Update ItemHandler.java

--- src/main/java/org/cubeville/cvcreativeutils/handlers/ItemHandler.java
+++ src/main/java/org/cubeville/cvcreativeutils/handlers/ItemHandler.java
@@ -2,6 +2,7 @@ package org.cubeville.cvcreativeutils.handlers;
 
 import net.minecraft.nbt.NBTTagCompound;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.block.Sign;
@@ -9,7 +10,10 @@ import org.bukkit.craftbukkit.v1_20_R2.inventory.CraftItemStack;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryCreativeEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.event.player.PlayerAttemptPickupItemEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.PotionMeta;
 import org.bukkit.inventory.meta.SpawnEggMeta;
@@ -22,19 +26,46 @@ import java.util.List;
 public class ItemHandler implements Listener {
 
     @EventHandler(priority = EventPriority.HIGHEST)
-    public void onInventoryCreative(InventoryCreativeEvent event) {
-        if(event.isCancelled()) return;
+    public void onInventoryCreative(InventoryClickEvent event) {
+        if(event.getClickedInventory() != null) checkEntireInventory(event.getClickedInventory());
+        checkEntireInventory(event.getInventory());
+        /*if(event.isCancelled()) return;
         ItemStack itemStack = event.getCursor();
         int byteCount = getByteCount(itemStack);
         if(byteCount == 0) return;
         if(byteCount > 100000 || isItemNBTBanned(itemStack)) {
             event.setCancelled(true);
-            ItemStack newItem = new ItemStack(itemStack.getType(), itemStack.getAmount());
-            if(event.getClickedInventory() != null) event.getClickedInventory().setItem(event.getSlot(), newItem);
+            if(event.getClickedInventory() != null) event.getClickedInventory().setItem(event.getSlot(), createNewStack(itemStack));
             event.getWhoClicked().sendMessage(ChatColor.RED + String.valueOf(itemStack.getType()) + " had a NBT value above what is allowed! NBT Cleared!");
-        }
+        }*/
     }
 
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onInventoryOpen(InventoryOpenEvent event) {
+        checkEntireInventory(event.getInventory());
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onItemPickup(PlayerAttemptPickupItemEvent event) {
+        if(event.isCancelled()) return;
+        checkEntireInventory(event.getPlayer().getInventory());
+    }
+
+    private ItemStack createNewStack(ItemStack itemStack) {
+        return new ItemStack(itemStack.getType(), itemStack.getAmount());
+    }
+
+    private void checkEntireInventory(Inventory inventory) {
+        int i = 0;
+        for(ItemStack itemStack : inventory.getContents()) {
+            if(itemStack != null && !itemStack.getType().equals(Material.AIR)) {
+                if(getByteCount(itemStack) > 100000|| isItemNBTBanned(itemStack)) {
+                    inventory.setItem(i, createNewStack(itemStack));
+                }
+            }
+            i++;
+        }
+    }
     private int getByteCount(ItemStack itemStack) {
         net.minecraft.world.item.ItemStack nmsItem = CraftItemStack.asNMSCopy(itemStack);
         NBTTagCompound nbtTagCompound = nmsItem.v();
@@ -65,9 +96,7 @@ public class ItemHandler implements Listener {
                 }
             }
         }
-        if(itemStack instanceof Sign) {
-            banned = true;
-        } else if(hasItemMeta && itemStack.getItemMeta() instanceof SpawnEggMeta) {
+        if(itemStack instanceof Sign || (hasItemMeta && itemStack.getItemMeta() instanceof SpawnEggMeta)) {
             banned = true;
         } else if(hasItemMeta && itemStack.getItemMeta() instanceof PotionMeta) {
             for(PotionEffect effect : ((PotionMeta)itemStack.getItemMeta()).getCustomEffects()) {
